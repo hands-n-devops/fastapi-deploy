3ccb1b6ef80878f94904535b09328ebd0a1b4f14
Vijay Gawte <vijaygawate79@gmail.com>
2026-07-30 13:21:31 +0530

    v3

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(
 @app.get("/")
 async def root():
     return {
-        "message": "Hello from FastAPI running on Azure App Service by Vijay-v2"
+        "message": "Hello from FastAPI running on Azure App Service by Vijay-v3"
     }
 
 
